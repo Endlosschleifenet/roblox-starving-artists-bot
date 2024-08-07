9476faac318f18ada8481247ef27d43b38c6afa3
Alixsandr <82512154+Endlosschleifenet@users.noreply.github.com>
2024-08-07 06:10:12 +0100

    Update build.bat
    Standalones now go in a new folder called executables if folder not already made. Also cleans up any residual *.spec files.

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
 # Use PyInstaller to create a standalone executable for bot.py
-pyinstaller --onefile bot.py
+
+mkdir -p executables
+
+pyinstaller --onefile --distpath executables bot.py
+pyinstaller --onefile --distpath executables coordinates-finder.py
+rm -rf build dist *.spec
